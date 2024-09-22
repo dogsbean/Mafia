6cae750822e8da931577c8e4e32d572134013190
dogsbean <lja0328@gmail.com>
2024-09-22 16:33:24 +0900

    Added arrestedCount in player profile

--- src/main/java/io/dogsbean/mafia/game/player/PlayerProfile.java
+++ src/main/java/io/dogsbean/mafia/game/player/PlayerProfile.java
@@ -1,5 +1,6 @@
 package io.dogsbean.mafia.game.player;
 
+import lombok.Getter;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
@@ -9,7 +10,8 @@ public class PlayerProfile {
     private static final Map<Player, PlayerProfile> profiles = new HashMap<>();
 
     private Player player;
-    private int money;
+    @Getter private int money;
+    @Getter private int arrestedCount;
 
     private PlayerProfile(Player player) {
         this.player = player;
@@ -22,10 +24,7 @@ public class PlayerProfile {
 
     public void initializeProfile() {
         this.money = 10000;
-    }
-
-    public int getMoney() {
-        return money;
+        this.arrestedCount = 0;
     }
 
     public void setMoney(int money) {
